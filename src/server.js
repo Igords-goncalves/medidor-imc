25fbc7e150a2bf03ac53ad128c4e19ad5869ea1a
Igor Gonçalves <igordossantos38@gmail.com>
2022-05-26 15:45:44 -0300

    Tentando realizar o deploy da aplicaçao no Heroku, porem um problema com reconhecimento do css impede a renderizaçao da aplicaçao, o arquivo html esta sendo lido corretamente

--- src/server.js
+++ src/server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const path = require('path');
-const router = express.Router();
 
 const app = express();
 
-router.get('/', (req,res) => {
-    res.sen(path.join(__dirname, 'index.html'))
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
   });
 
-app.use('/', router);
 
 const PORT = process.env.PORT || 5000;
 
