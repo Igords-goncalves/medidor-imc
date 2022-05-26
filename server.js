1cff6a2d985a67254e2d764c63f9805a0cc2717e
Igor Gonçalves <igordossantos38@gmail.com>
2022-05-26 09:44:54 -0300

    Organizando e estruturando aplicaçao para deploy, criando servidor express

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const path = require('path');
+const router = express.Router();
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.sendFile('./index.html');
-});
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,'/index.html'));
+  });
+
+
+app.use('/', router);
 
 const PORT = process.env.PORT || 5000;
 
